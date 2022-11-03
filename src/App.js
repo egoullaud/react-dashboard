dfa709d711f2e79ed6a040bb76c664f5b2a85a38
elizabeth.goullaud <egoullaud@gmail.com>
2022-11-03 16:19:21 -0700

    added routes to app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@
 import {ColorModeContext, useMode} from './theme'
 //baseline resets css to defaults, themeprovider gives ability to pass themes to mui
 import {CssBaseline, ThemeProvider} from '@mui/material'
+import {Routes, Route} from 'react-router-dom'
+//import scenes
 import Topbar from './scenes/global/Topbar';
+import Sidebar from './scenes/global/Sidebar'
+//pages
+import Dashboard from './scenes/dashboard';
+// import Team from './scenes/team';
+// import Bar from './scenes/bar';
+// import Invoices from './scenes/invoices';
+// import Contacts from './scenes/contacts';
+// import Form from './scenes/form';
+// import Line from './scenes/line';
+// import Pie from './scenes/pie';
+// import FAQ from './scenes/faq';
+// import Geography from './scenes/geography';
+// import Calendar from './scenes/calendar';
+
+
+
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -15,8 +33,22 @@ function App() {
         {/* reset css to defaults */}
         <CssBaseline>
           <div className="app">
+            <Sidebar/>
             <main className='content'>
              <Topbar/> 
+             <Routes>
+              <Route pat="/" element={<Dashboard/>}/>
+              {/* <Route path="/team" element={<Team/>}/> */}
+              {/* <Route path="/contacts" element={<Invoices/>}/> */}
+              {/* <Route path="/invoices" element={<Contacts/>}/> */}
+              {/* <Route path="/form" element={<Form/>}/> */}
+              {/* <Route path="/bar" element={<Bar/>}/> */}
+              {/* <Route path="/pie" element={<Pie/>}/> */}
+              {/* <Route path="/line" element={<Line/>}/> */}
+              {/* <Route path="/faq" element={<FAQ/>}/> */}
+              {/* <Route path="/geography" element={<Geography/>}/> */}
+              {/* <Route path="/calendar" element={<Calendar/>}/> */}
+             </Routes>
             </main>
           </div>
          </CssBaseline>
