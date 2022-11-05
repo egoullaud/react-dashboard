145ccdcbe3918ddb7a3bfec39fff9f6c6008cff1
elizabeth.goullaud <egoullaud@gmail.com>
2022-11-05 16:24:33 -0700

    added user form

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Team from './scenes/team';
 // import Bar from './scenes/bar';
 import Invoices from './scenes/invoices';
 import Contacts from './scenes/contacts';
-// import Form from './scenes/form';
+import Form from './scenes/form';
 // import Line from './scenes/line';
 // import Pie from './scenes/pie';
 // import FAQ from './scenes/faq';
@@ -41,7 +41,7 @@ function App() {
               <Route path="/team" element={<Team/>}/>
               <Route path="/contacts" element={<Contacts/>}/>
               <Route path="/invoices" element={<Invoices/>}/>
-              {/* <Route path="/form" element={<Form/>}/> */}
+              <Route path="/form" element={<Form/>}/>
               {/* <Route path="/bar" element={<Bar/>}/> */}
               {/* <Route path="/pie" element={<Pie/>}/> */}
               {/* <Route path="/line" element={<Line/>}/> */}
