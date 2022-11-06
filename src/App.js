6788c01387d192fa8c433db2d101e04b2c0d80cb
elizabeth.goullaud <egoullaud@gmail.com>
2022-11-05 17:07:51 -0700

    added calendar

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import Form from './scenes/form';
 // import Pie from './scenes/pie';
 // import FAQ from './scenes/faq';
 // import Geography from './scenes/geography';
-// import Calendar from './scenes/calendar';
+import Calendar from './scenes/calendar';
 
 
 
@@ -47,7 +47,7 @@ function App() {
               {/* <Route path="/line" element={<Line/>}/> */}
               {/* <Route path="/faq" element={<FAQ/>}/> */}
               {/* <Route path="/geography" element={<Geography/>}/> */}
-              {/* <Route path="/calendar" element={<Calendar/>}/> */}
+              <Route path="/calendar" element={<Calendar/>}/>
              </Routes>
             </main>
           </div>
