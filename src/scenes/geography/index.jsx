e8598fb26719f0a7c2d231790701b0b250f94b4c
elizabeth.goullaud <egoullaud@gmail.com>
2022-11-08 14:54:19 -0800

    added charts, now assembling dashboard

--- src/scenes/geography/index.jsx
+++ src/scenes/geography/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {Box} from '@mui/material'
+import Header from '../../components/Header'
+import GeoChart from '../../components/GeoChart'
+import {useTheme} from '@mui/material'
+import {tokens} from "../../theme"
 
-export default function Geography() {
+function Geography() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
-    <div>
-      
-    </div>
+    <Box m="20px">
+      <Header title="Geography Chart" subtitle="Simple Geography Chart"/>
+      <Box height="75vh" 
+      border={`1px solid ${colors.grey[100]}`} 
+      borderRadius="4px">
+      <GeoChart/>
+      </Box>
+    </Box>
   )
 }
+
+export default Geography
