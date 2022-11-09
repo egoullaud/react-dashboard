795765fab590d4f1b786822135aeac9a9bfe4fdc
elizabeth.goullaud <egoullaud@gmail.com>
2022-11-08 16:10:10 -0800

    dashboard finished, app done!

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -180,19 +180,21 @@ function Dashboard() {
                         borderBottom={`4px solid ${colors.primary[500]}`}
                         p="15px"
                       >
-                      <Box> 
-                          <Typography
-                            color={colors.greenAccent[500]} 
-                            variant="h5" 
-                            fontWeight="600">
-                              {transaction.txId}
-                          </Typography>
-                          <Typography 
-                            color={colors.grey[100]}>
-                              {transaction.user}
-                          </Typography>
-                    </Box>
-                    <Box color={colors.grey[100]}>{transaction.date} </Box>
+                        <Box> 
+                            <Typography
+                              color={colors.greenAccent[500]} 
+                              variant="h5" 
+                              fontWeight="600">
+                                {transaction.txId}
+                            </Typography>
+                            <Typography 
+                              color={colors.grey[100]}>
+                                {transaction.user}
+                            </Typography>
+                        </Box>
+                        <Box color={colors.grey[100]}>
+                          {transaction.date} 
+                        </Box>
                     <Box 
                       backgroundColor={colors.greenAccent[500]} 
                       p="5px 10px" 
@@ -203,8 +205,81 @@ function Dashboard() {
                 ))}
               </Box>
 {/* ROW 3 */}
-
-
+                  <Box
+                    gridColumn="span 4"
+                    gridRow="span 2"
+                    backgroundColor={colors.primary[400]}
+                    p="30px"
+                  >
+                      <Typography
+                        variant="h5"
+                        fontWeight="600"
+                      >
+                        Campaign
+                      </Typography>
+                      <Box
+                        display="flex"
+                        flexDirection="column"
+                        alignItems="center"
+                        mt="25px"
+                      >
+                        <ProgressCircle size="125" />
+                        <Typography
+                          variant="h5"
+                          color={colors.greenAccent[500]}
+                          sx={{ mt: "15px"}}
+                        >
+                          $48,352 Revenue Generated
+                        </Typography>
+                        <Typography>
+                            Includes extra misc expenditures and costs
+                        </Typography>
+                      </Box>
+                  </Box>
+                  <Box
+                    gridColumn="span 4"
+                    gridRow="span 2"
+                    backgroundColor={colors.primary[400]}
+                   
+                  >
+                      <Typography
+                        variant="h5"
+                        fontWeight="600"
+                        sx={{p:"30px 30px 0 30px"}}
+                      >
+                        Sales Quantity
+                      </Typography>
+                      <Box
+                       height="250px"
+                        mt="-20px"
+                      >
+                        <BarChart isDashboard={true}/>
+                        
+                      </Box>
+                  </Box>
+                  <Box
+                    gridColumn="span 4"
+                    gridRow="span 2"
+                    backgroundColor={colors.primary[400]}
+                    p="30px"
+                   
+                  >
+                      <Typography
+                        variant="h5"
+                        fontWeight="600"
+                        sx={{mb:"15px"}}
+                      >
+                        Geography Based Traffic
+                      </Typography>
+                      <Box
+                       height="200px"
+                      
+                      >
+                        <GeoChart isDashboard={true}/>
+                        
+                      </Box>
+                  </Box>
+{/*  */}
           </Box>
         </Box>
     
